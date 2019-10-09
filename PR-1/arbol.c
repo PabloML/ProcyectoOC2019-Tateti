fa3bfafdee2b40cd38b202b7bc3ecbc15a8d9274
Nano3114 <54462455+Nano3114@users.noreply.github.com>
2019-10-09 15:29:38 -0300

    Add files via upload
    arbol.c

---
+++ PR-1/arbol.c
@@ -0,0 +1,12 @@
+/**
+ * TDA Arbol.
+ * @author Pablo Lencina, Adrian Flores
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+#define ARB_OPERACION_INVALIDA      10
+#define ARB_POSICION_INVALIDA       11
+#define ARB_ERROR_MEMORIA           12
